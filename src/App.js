2da3bdb9c2ea96c20259e4a3af46a8081d7bbf3f
Guy Lepage <guylepage3@gmail.com>
2018-10-01 17:46:35 -0400

    built out new Header component #7
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
 import './styles/Universe.css';
 import './styles/App.css';
 
@@ -6,24 +7,23 @@ class App extends Component {
   render() {
     return (
       <div className="App d-flex w-100 h-100 mx-auto p-4 flex-column">
-        <div>
-          <div className="mb-auto"
-            style={{
-              paddingTop: '80px'
-            }}>
-          </div>
-          <main role="main" className="mx-auto text-center content-hero"
-            style={{
-              maxWidth: '685px'
-            }}>
-            <h1 className="display-2 text-white font-weight-bold">
-              Universe
-            </h1>
-            <div className="lead text-white-50">
-              Universe is a blockchain agnostic dApp solutions community
-            </div>
-          </main>
+        <Header />
+        <div className=""
+          style={{
+            paddingTop: '65px'
+          }}>
         </div>
+        <main role="main" className="mx-auto text-center content-hero"
+          style={{
+            maxWidth: '685px'
+          }}>
+          <h1 className="display-2 text-white font-weight-bold">
+            Universe
+          </h1>
+          <div className="lead text-white-50">
+            Universe is a blockchain agnostic dApp solutions community
+          </div>
+        </main>
       </div>
     );
   }
