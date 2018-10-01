a69c37ab3cedbd38b93793607c33c87e79526afb
Guy Lepage <guylepage3@gmail.com>
2018-10-01 17:17:47 -0400

    added full page cover wrapper #7
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,13 +5,7 @@ import './styles/App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App d-flex w-100 h-100 mx-auto p-4 flex-column">
       </div>
     );
   }
