11e3e7dd746f83fc41ad0e3ea27f00863402e5a5
Guy Lepage <guylepage3@gmail.com>
2018-10-01 17:20:18 -0400

    added quick headline w/ description #7
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App d-flex w-100 h-100 mx-auto p-4 flex-column">
+        <h1 className="display-2 text-white font-weight-bold">
+          Universe
+        </h1>
+        <div className="lead text-white-50">
+          Universe is a blockchain agnostic dApp solutions community
+        </div>
       </div>
     );
   }
