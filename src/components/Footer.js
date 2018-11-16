601f5c67aec327614d71a1e036c9bd4e4aaf4a50
Guy Lepage <guylepage3@gmail.com>
2018-11-15 21:18:42 -0500

    applied font awesome to footer #32
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import logoCluster from './../img/universe-mite-icon-rounded.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Footer extends Component {
   render() {
     return (
-      <footer className="mx-auto text-center mb-2">
-        <div className="container-fluid mx-auto mb-4 d-none d-md-block">
+      <footer className="container-fluid mx-auto text-center mb-2">
+        <div className="text-white-50 text-footer font-weight-bold">
           <img src={logoCluster}
             alt="Universe logo cluster"
             width="45"
@@ -14,21 +15,40 @@ class Footer extends Component {
             }}
           />
         </div>
-        <div className="mb-2 text-white-50 text-footer font-weight-bold">
-          <a href="https://github.com/universelabs/" className="text-white">
-            GitHub
-          </a>
-          &nbsp;&nbsp;&middot;&nbsp;&nbsp;
-          <a href="https://twitter.com/UniverseLabs" className="text-white">
-            Twitter
-          </a>
-          &nbsp;&nbsp;&middot;&nbsp;&nbsp;
-          <a href="https://join.slack.com/t/universelabs/shared_invite/enQtNDQ0MjY3NDI5MTkwLTIzMWQ4M2U3MGQ3ZDY5MzM5MGQ5ZDM1MDZjNTgwNGI5NDdiNDY4ZDQyNWI2NjEzZmU3NzVmOTYwYzEzYzc1ZDE" className="text-white">
-            Slack
-          </a>
+        <div>
+          <ul className="list-inline my-2">
+            <li className="list-inline-item mx-2">
+              <a href="https://github.com/universelabs/" 
+                alt="GitHub UniverseLabs (Universe Labs)"
+                className="text-white">
+                <FontAwesomeIcon className="mr-1" icon={['fab', 'github']} /> GitHub
+              </a>
+            </li>
+            <li className="list-inline-item mx-2">
+              <a href="https://twitter.com/UniverseLabs" 
+                alt="Twitter Universe Labs (@UniverseLabs)"
+                className="text-white">
+                <FontAwesomeIcon className="mr-1" icon={['fab', 'twitter']} /> Twitter
+              </a>
+            </li>
+            <li className="list-inline-item mx-2">
+              <a href="https://medium.com/@universelabs" 
+                alt="Universe Labs – Medium"
+                className="text-white">
+                <FontAwesomeIcon className="mr-1" icon={['fab', 'linkedin']} /> Medium
+              </a>
+            </li>
+            <li className="list-inline-item mx-2">
+              <a href="https://join.slack.com/t/universelabs/shared_invite/enQtNDQ0MjY3NDI5MTkwLTIzMWQ4M2U3MGQ3ZDY5MzM5MGQ5ZDM1MDZjNTgwNGI5NDdiNDY4ZDQyNWI2NjEzZmU3NzVmOTYwYzEzYzc1ZDE" 
+                alt="Universe Labs – Slack"
+                className="text-white">
+                <FontAwesomeIcon className="mr-1" icon={['fab', 'medium']} /> Slack
+              </a>
+            </li>
+          </ul>
         </div>
         <div className="text-footer text-white-50 font-weight-normal">
-          Universe Labs Inc. &copy; 2018.
+          Universe Labs &copy; 2018.
         </div>
       </footer>
     );
