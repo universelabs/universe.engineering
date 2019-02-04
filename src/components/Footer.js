069cfbb9dbc268c3662e0ceb2ea109fa9ac182c9
Guy Lepage <guylepage3@gmail.com>
2019-02-04 16:47:11 -0500

    udpate to footer
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+import CopyrightUpdate from 'copyright-update';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Footer extends Component {
   render() {
     return (
-      <footer className="container-fluid mx-auto text-center mb-2">
+      <footer className="container-fluid mx-auto text-center pb-4">
         <div className="text-white-50 text-footer font-weight-bold">
-        </div>
-        <div>
           <ul className="list-inline my-2">
             <li className="list-inline-item mx-2">
               <a href="https://github.com/universelabs/" 
@@ -27,20 +26,34 @@ class Footer extends Component {
               <a href="https://medium.com/@universelabs" 
                 alt="Universe Labs – Medium"
                 className="text-white">
-                <FontAwesomeIcon className="mr-1" icon={['fab', 'linkedin']} /> Medium
+                <FontAwesomeIcon className="mr-1" icon={['fab', 'medium']} /> Medium
               </a>
             </li>
             <li className="list-inline-item mx-2">
               <a href="https://join.slack.com/t/universelabs/shared_invite/enQtNDQ0MjY3NDI5MTkwLTIzMWQ4M2U3MGQ3ZDY5MzM5MGQ5ZDM1MDZjNTgwNGI5NDdiNDY4ZDQyNWI2NjEzZmU3NzVmOTYwYzEzYzc1ZDE" 
                 alt="Universe Labs – Slack"
                 className="text-white">
-                <FontAwesomeIcon className="mr-1" icon={['fab', 'medium']} /> Slack
+                <FontAwesomeIcon className="mr-1" icon={['fab', 'slack']} /> Slack
               </a>
             </li>
           </ul>
         </div>
+        <div className="text-footer text-white-50 font-weight-normal my-1">
+          <a 
+            href="mailto:support@universe.engineering?subject=Mail from universe.engineering" 
+            alt="Contact Universe Labs"
+            className="text-white font-weight-bold">
+            Contact
+          </a>
+        </div>
         <div className="text-footer text-white-50 font-weight-normal">
-          Universe Labs &copy; 2018.
+          Universe Labs Inc. 
+            <CopyrightUpdate 
+              style={{
+                display: 'inline-block',
+                marginLeft: '6px'
+              }}
+            />
         </div>
       </footer>
     );
