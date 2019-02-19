f0a1cec10425433b55bbf74d166872647d95ee6b
Guy Lepage <guylepage3@gmail.com>
2019-02-19 06:18:12 -0500

    update to react router
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import NoMatch from './containers/NoMatch';
 import Subscribe from './containers/Subscribe';
 import SubscribeError from './containers/SubscribeError';
 import SubscribeSuccess from './containers/SubscribeSuccess';
-import { BrowserRouter, Route } from 'react-router-dom';
+import {BrowserRouter, Switch, Route } from 'react-router-dom';
 import './css/App.css';
 
 class App extends Component {
@@ -22,16 +22,18 @@ class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Route exact path="/" component={Home} />
-          <Route path="/subscribe" component={Subscribe} />
-          <Route path="/subscribe-error" component={SubscribeError} />
-          <Route path="/subscribe-success" component={SubscribeSuccess} />
-          {/* when none of the above match, <NoMatch> will be rendered */}
-          <Route component={NoMatch} />
-        </div>
-      </BrowserRouter>
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/subscribe" component={Subscribe} />
+            <Route path="/subscribe-error" component={SubscribeError} />
+            <Route path="/subscribe-success" component={SubscribeSuccess} />
+            {/* when none of the above match, <NoMatch> will be rendered */}
+            <Route component={NoMatch} />
+          </Switch>
+        </BrowserRouter>
+      </div>
     );
   }
 }
