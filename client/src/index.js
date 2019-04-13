16f1458c5d422fd4f9bb054f0b597793e4e58501
Guy Lepage <guylepage3@gmail.com>
2019-04-13 17:13:39 -0400

    resolved navbar hover issue #44
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import './scss/universe-styles.scss';
+import './scss/universe.scss';
 import './css/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
