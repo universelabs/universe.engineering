3a6dc454e2cfba3842829bb3c851e01c64eefd76
Guy Lepage <guylepage3@gmail.com>
2019-05-01 12:00:28 -0400

    refactored keylist closes#51
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- client/src/components/KeyList.js
+++ client/src/components/KeyList.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 
 class KeyList extends Component {
+  state = {
+    data : [
+      {
+        id: 1,
+        href: '#',
+        text: 'Blockstack Keys'
+      },
+      {
+        id: 2,
+        href: '#',
+        text: 'Solid Keys'
+      },
+      {
+        id: 3,
+        href: '#',
+        text: 'MetaMask (Ethereum) Keys'
+      }
+    ]
+  }
+  
   render() {
-    return (
-      <div className="list-group">
-        <a href="#" className="list-group-item list-group-item-action">
-          Blockstack Keys
+    return this.state.data.map((item) => (
+      <li className="list-group-item list-group-item-action" 
+        key={item.id} 
+        style={{
+          cursor: 'pointer'
+        }}>
+        <a 
+          href={item.url} 
+          alt="Key, passphrase or seed">
+          {item.text}
         </a>
-        <a href="#" className="list-group-item list-group-item-action">Solid Keys</a>
-        <a href="#" className="list-group-item list-group-item-action">MetaMask (Ethereum) Keys</a>
-      </div>
-    );
+      </li>
+    ));
   }
 }
 
--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -14,28 +14,41 @@ class Dashboard extends Component {
         style={{
           backgroundColor: '#FAFAFA'
         }}>
-        <NavbarLight colorScheme="navbar-light bg-light bg-white"/>
+        <NavbarLight 
+          colorScheme="
+            navbar-light 
+            bg-light 
+            bg-white"
+          />
         <Alert
           size=""
           color="alert-danger"
           className="text-center"
           style={{
             marginBottom: 0
-          }}
-          >
+          }}>
           <strong>WARNING:</strong> Product in Alpha - <strong>DO NOT</strong> store more than <strong>$10</strong> on your keys.
         </Alert>
-        <div style={{ marginLeft: '6px', marginRight: '6px' }}>
+        <div 
+          style={{
+            marginLeft: '6px',
+            marginRight: '6px'
+          }}>
           <div 
-            className="container text-left mb-8 pb-8">
-            <div></div>
+            className="
+              container 
+              text-left 
+              mb-8 pb-8"
+            >
             <h3 className="h1-editorial mb-4b">
               Your Keys
             </h3>
             <p className="mb-5">
               Your keys for your various platforms are below.
             </p>
-            <KeyList />
+            <div className="list-group">
+              <KeyList />
+            </div>
           </div>
         </div>
       </div>
